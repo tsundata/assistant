72760a8b1bd8920787c0eeb68a6284debb8b399b
sysatom <sysatom@gmail.com>
2020-11-02 12:01:32 +0800

    fix: update rpc client

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/spf13/viper v1.7.1
 	github.com/tsundata/framework v0.0.0-20201028030150-d28aa69b01ec
-	github.com/tsundata/rpc v0.0.0-20201031082608-e65c8e4dd08e
+	github.com/tsundata/rpc v0.0.0-20201102032540-781e5f809991
 	google.golang.org/protobuf v1.25.0
 	gorm.io/driver/mysql v1.0.3
 	gorm.io/gorm v1.20.5
--- internal/pkg/transports/rpc/client.go
+++ internal/pkg/transports/rpc/client.go
@@ -2,6 +2,7 @@ package rpc
 
 import (
 	"context"
+	"errors"
 	"github.com/spf13/viper"
 	"github.com/tsundata/rpc/xclient"
 	"time"
@@ -36,27 +37,41 @@ func WithTimeout(d time.Duration) ClientOptional {
 
 type Client struct {
 	o  *ClientOptions
-	xc *xclient.XClient
+	xc map[string]*xclient.XClient
 }
 
 func NewClient(o *ClientOptions) (*Client, error) {
-	d := xclient.NewRegistryDiscovery(o.Registry, 0)
-	xc := xclient.NewXClient(d, xclient.RandomSelect, nil)
-
 	return &Client{
-		o:  o,
-		xc: xc,
+		o: o,
 	}, nil
 }
 
-func (c *Client) Call(ctx context.Context, serviceMethod string, args, reply interface{}) error {
-	return c.xc.Call(ctx, serviceMethod, args, reply)
+func (c *Client) Call(ctx context.Context, servicePath, serviceMethod string, args, reply interface{}) error {
+	xc, ok := c.xc[servicePath]
+	if !ok {
+		d := xclient.NewRegistryDiscovery(c.o.Registry, c.o.Wait)
+		xc = xclient.NewXClient(servicePath, d, xclient.RandomSelect, nil)
+	}
+
+	return xc.Call(ctx, serviceMethod, args, reply)
 }
 
-func (c *Client) Broadcast(ctx context.Context, serviceMethod string, args, reply interface{}) error {
-	return c.xc.Broadcast(ctx, serviceMethod, args, reply)
+func (c *Client) Broadcast(ctx context.Context, servicePath, serviceMethod string, args, reply interface{}) error {
+	xc, ok := c.xc[servicePath]
+	if !ok {
+		d := xclient.NewRegistryDiscovery(c.o.Registry, c.o.Wait)
+		xc = xclient.NewXClient(servicePath, d, xclient.RandomSelect, nil)
+	}
+
+	return xc.Broadcast(ctx, serviceMethod, args, reply)
 }
 
-func (c *Client) Close() error {
-	return c.xc.Close()
+func (c *Client) Close(servicePath string) error {
+	xc, ok := c.xc[servicePath]
+	if ok {
+		delete(c.xc, servicePath)
+		return xc.Close()
+	}
+
+	return errors.New("error xc")
 }
