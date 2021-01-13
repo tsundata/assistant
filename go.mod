49780d8d7d0562bacad2bb8c65c41c27ee0ea875
sysatom <sysatom@gmail.com>
2021-01-13 11:28:03 +0800

    feat: server agent

--- go.mod
+++ go.mod
@@ -8,19 +8,16 @@ require (
 	github.com/gogo/protobuf v1.3.1
 	github.com/gorhill/cronexpr v0.0.0-20180427100037-88b0669f7d75
 	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2
-	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
 	github.com/grpc-ecosystem/grpc-opentracing v0.0.0-20180507213350-8e809c8a8645
 	github.com/influxdata/influxdb-client-go/v2 v2.2.1
 	github.com/opentracing/opentracing-go v1.2.0
-	github.com/prometheus/client_golang v1.8.0
 	github.com/robertkrimen/otto v0.0.0-20200922221731-ef014fd054ac
 	github.com/shirou/gopsutil v3.20.12+incompatible // indirect
+	github.com/shirou/gopsutil/v3 v3.20.12
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	github.com/slack-go/slack v0.7.2
-	github.com/smallnest/rpcx v0.0.0-20201207055143-dff6bb8dd30b
 	github.com/spf13/viper v1.7.1
 	github.com/uber/jaeger-client-go v2.25.0+incompatible
-	github.com/uber/jaeger-lib v2.4.0+incompatible
 	github.com/valyala/fasthttp v1.17.0
 	go.etcd.io/etcd v0.0.0-20201125193152-8a03d2e9614b
 	go.uber.org/zap v1.16.0
--- internal/app/agent/broker/server.go
+++ internal/app/agent/broker/server.go
@@ -3,8 +3,11 @@ package broker
 import (
 	"fmt"
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/disk"
+	"github.com/shirou/gopsutil/v3/host"
+	"github.com/shirou/gopsutil/v3/mem"
 	"go.uber.org/zap"
-	"math/rand"
 	"time"
 )
 
@@ -25,14 +28,47 @@ func (b *Server) Init(org string, bucket string, logger *zap.Logger, influx infl
 
 func (b *Server) Run() {
 	t := time.NewTicker(time.Second)
+	// host
+	h, err := host.Info()
+	if err != nil {
+		b.logger.Error(err.Error())
+		return
+	}
 	writeAPI := b.influx.WriteAPI(b.org, b.bucket)
 	for {
 		select {
 		case <-t.C:
-			rand.Seed(time.Now().Unix())
-			row := fmt.Sprintf("stat,unit=temperature avg=%d,max=%d", rand.Intn(100), rand.Intn(100)+10)
-			b.logger.Info(row)
-			writeAPI.WriteRecord(row)
+			// mem
+			v, err := mem.VirtualMemory()
+			if err != nil {
+				b.logger.Error(err.Error())
+				continue
+			}
+			// swap
+			s, err := mem.SwapMemory()
+			if err != nil {
+				b.logger.Error(err.Error())
+				continue
+			}
+			// cpu
+			c, err := cpu.Percent(time.Second, false)
+			if err != nil {
+				b.logger.Error(err.Error())
+				continue
+			}
+			// disk
+			d, err := disk.Usage("/")
+			if err != nil {
+				b.logger.Error(err.Error())
+				continue
+			}
+
+			for i, ci := range c {
+				writeAPI.WriteRecord(fmt.Sprintf("cpu_stat,host=%s,cpu=%d cpu_used=%f", h.Hostname, i+1, ci))
+			}
+			writeAPI.WriteRecord(fmt.Sprintf("mem_stat,host=%s mem_total=%v,mem_free=%v,mem_used=%f", h.Hostname, v.Total, v.Free, v.UsedPercent))
+			writeAPI.WriteRecord(fmt.Sprintf("disk_stat,host=%s disk_total=%d,disk_free=%d,disk_used=%f", h.Hostname, d.Total, d.Free, d.UsedPercent))
+			writeAPI.WriteRecord(fmt.Sprintf("swap_stat,host=%s swap_total=%d,swap_free=%d,swap_used=%f", h.Hostname, s.Total, s.Free, s.UsedPercent))
 			writeAPI.Flush()
 		}
 	}
