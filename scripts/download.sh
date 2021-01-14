a6494abb73a4312742ffe1f404dd08c72f021dd2
sysatom <sysatom@gmail.com>
2021-01-14 15:52:50 +0800

    feat: download script

---
+++ scripts/download.sh
@@ -0,0 +1,16 @@
+echo "version $1"
+mkdir -p "dist"
+# apps
+for app in 'gateway' 'message' 'subscribe' 'web' 'middle' 'spider' 'cron'
+	do
+	  echo "\n downloading... $app\n"
+		curl -L https://github.com/tsundata/assistant/releases/download/v$1/$app-linux-amd64 --output ./dist/$app-linux-amd64
+		chmod +x ./dist/$app-linux-amd64
+  done
+# agents
+for agent in 'server'
+	do
+	  echo "\n downloading... $agent\n"
+		curl -L https://github.com/tsundata/assistant/releases/download/v$1/$agent-agent-linux-amd64 --output ./dist/$agent-agent-linux-amd64
+		chmod +x ./dist/$agent-agent-linux-amd64
+	done
