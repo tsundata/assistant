0ee265b9cdbce501d70b94af9df1b09e00f30757
sysatom <sysatom@gmail.com>
2021-01-26 14:13:08 +0800

    opt: spider

--- internal/pkg/utils/slice.go
+++ internal/pkg/utils/slice.go
@@ -1,6 +1,9 @@
 package utils
 
-func SliceDiff(s1, s2 []string) []string {
+func StringSliceDiff(s1, s2 []string) []string {
+	if len(s1) == 0 {
+		return s2
+	}
 	mb := make(map[string]struct{}, len(s2))
 	for _, x := range s2 {
 		mb[x] = struct{}{}
