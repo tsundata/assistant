8c2ecfa260295b1c64eeccffd02575c9f3607a4a
sysatom <sysatom@gmail.com>
2021-01-26 14:15:10 +0800

    fix: tests

--- internal/pkg/utils/slice_test.go
+++ internal/pkg/utils/slice_test.go
@@ -7,13 +7,13 @@ import (
 func TestSliceDiff(t *testing.T) {
 	s1 := []string{"a", "b", "c"}
 	s2 := []string{"c", "d", "a"}
-	diff := SliceDiff(s1, s2)
+	diff := StringSliceDiff(s1, s2)
 	if len(diff) != 1 && diff[0] != "b" {
 		t.Fatal("error: slice diff")
 	}
 
 	var s3 []string
-	diff = SliceDiff(s1, s3)
+	diff = StringSliceDiff(s1, s3)
 	if len(diff) != 3 {
 		t.Fatal("error: slice diff")
 	}
