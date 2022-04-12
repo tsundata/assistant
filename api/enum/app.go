ac660e6cf071300d5cd8b98383f19c727cced718
sysatom <sysatom@gmail.com>
2022-04-12 09:46:19 +0800

    refactor: fmt -w

--- api/enum/app.go
+++ api/enum/app.go
@@ -1,14 +1,14 @@
 package enum
 
 const (
-	Id       = "id"
-	Chatbot  = "chatbot"
-	Message  = "message"
-	Gateway  = "gateway"
-	Cron     = "cron"
-	Middle   = "middle"
-	Spider   = "spider"
-	Storage  = "storage"
-	Task     = "task"
-	User     = "user"
+	Id      = "id"
+	Chatbot = "chatbot"
+	Message = "message"
+	Gateway = "gateway"
+	Cron    = "cron"
+	Middle  = "middle"
+	Spider  = "spider"
+	Storage = "storage"
+	Task    = "task"
+	User    = "user"
 )
